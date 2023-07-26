bbc98026c09aa0615fc7cf804a245901e9f31107
bete7512 <betekbebe@gmail.com>
2023-07-26 08:59:53 +0300

    add to message

--- src/configuration/keys.js
+++ src/configuration/keys.js
@@ -1,6 +1,19 @@
-import dotenv from 'dotenv'
-dotenv.config()
+// import dotenv from 'dotenv'
+// dotenv.config()
+// Fixing the process object
+const process = {
+  env: {
+    JWT_SECRET: "9e1LrqyH4tI0RiUW45wYAQpyWaJ75uCefvq0XqwHtC8",
+    PORT: "3000",
+    DB_PORT: "3306",
+    DB_HOST: "109.70.148.67",
+    DB_PASSWORD: "G3rW8eT2wD0zK6i",
+    DB_NAME: "roblassupplychai_roblas_db",
+    DB_USER: "roblassupplychai_roblassupplychai",
+  },
+};
 
+// Accessing environment variables correctly
 const config = {
   DB_PORT: process.env.DB_PORT,
   DB_HOST: process.env.DB_HOST,
@@ -8,9 +21,9 @@ const config = {
   DB_NAME: process.env.DB_NAME,
   DB_USER: process.env.DB_USER,
   server: {
-    PORT:3000,
-    JWT_SECRET:process.env.JWT_SECRET
+    PORT: parseInt(process.env.PORT), // Convert PORT to an integer
+    JWT_SECRET: process.env.JWT_SECRET,
   },
-}
+};
 
 export default config
