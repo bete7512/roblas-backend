50e9356e6dfc6b71c86cea6516fd085adf93cfae
bete7512 <betekbebe@gmail.com>
2023-07-26 08:50:41 +0300

    connection sent

--- src/server.js
+++ src/server.js
@@ -1,14 +1,14 @@
 import express from 'express'
 import bodyParser from 'body-parser'
 import cors from 'cors'
-import initModels from './models/init-models'
+// import initModels from './models/init-models'
 import { sequelize } from './configuration/db'
 import config from './configuration/keys'
 import auth_route from './routes/auth'
 import recource_route from './routes/routes'
 const app = express()
 
-const models = initModels(sequelize)
+// const models = initModels(sequelize)
 app.use(express.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cors())
@@ -19,12 +19,8 @@ app.use('/api/', recource_route)
 //   alter: true,
 //   force: true,
 // }
-sequelize
-  .sync()
-  .then(() => {
-    app.listen(config.server.PORT, () => {
-      console.log(
-        `Example app listening at http://localhost:${config.server.PORT}`,
-      )
-    })
+sequelize.sync().then(() => {
+  app.listen(config.server.PORT, () => {
+    console.log(`Example app listening at http://localhost:${config.server.PORT}`)
   })
+})
