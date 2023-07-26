a4dd887f9b52f161200f7bcf70e2a4edf5500d2f
bete7512 <betekbebe@gmail.com>
2023-07-26 11:27:42 +0300

    add

--- configuration/keys.js
+++ configuration/keys.js
@@ -1,6 +1,16 @@
-var dotenv = require('dotenv');
-dotenv.config();
-
+// var dotenv = require('dotenv');
+// dotenv.config();
+const process = {
+  env: {
+    JWT_SECRET: "9e1LrqyH4tI0RiUW45wYAQpyWaJ75uCefvq0XqwHtC8",
+    PORT: "3000",
+    DB_PORT: "3306",
+    DB_HOST: "109.70.148.67",
+    DB_PASSWORD: "G3rW8eT2wD0zK6i",
+    DB_NAME: "roblassupplychai_roblas_db",
+    DB_USER: "roblassupplychai_roblassupplychai",
+  },
+};
 var config = {
   DB_PORT: process.env.DB_PORT,
   DB_HOST: process.env.DB_HOST,
